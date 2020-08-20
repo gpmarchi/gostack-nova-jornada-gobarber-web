192819c244929c83306fcb88e66873d472910805
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-08-20 14:40:40 -0300

    Provide toast functionality through context hook

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,13 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
-import ToastContainer from './components/ToastContainer';
-import { AuthProvider } from './context/AuthContext';
+import AppProvider from './context';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
-    </AuthProvider>
-
-    <ToastContainer />
+    </AppProvider>
 
     <GlobalStyle />
   </>
