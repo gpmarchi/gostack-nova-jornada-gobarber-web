8133fe1023a4c06967d7da4288145fd735885b6e
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-10-13 12:58:27 -0300

    Add tests for reset password page

---
+++ src/__tests__/pages/ResetPassword.spec.tsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import MockAdapter from 'axios-mock-adapter';
+
+import api from '../../services/api';
+
+import ResetPassword from '../../pages/ResetPassword';
+
+const apiMock = new MockAdapter(api);
+
+const mockedHistoryPush = jest.fn();
+let mockedUrlSearchString = '?token=jdfh-skjh1d-f123ads';
+const mockedAddToast = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    useLocation: () => ({
+      search: mockedUrlSearchString,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+jest.mock('../../context/ToastContext', () => {
+  return {
+    useToast: () => ({
+      addToast: mockedAddToast,
+    }),
+  };
+});
+
+describe('ResetPassword Page', () => {
+  beforeEach(() => {
+    mockedHistoryPush.mockClear();
+    mockedAddToast.mockClear();
+  });
+
+  it('should be able to reset password', async () => {
+    apiMock.onPost('/password/reset').replyOnce(204);
+
+    const { getByPlaceholderText, getByText } = render(<ResetPassword />);
+
+    const passwordField = getByPlaceholderText('Nova senha');
+    const passwordConfirmationField = getByPlaceholderText(
+      'Confirmação da senha',
+    );
+    const buttonElement = getByText('Alterar senha');
+
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+    fireEvent.change(passwordConfirmationField, {
+      target: { value: '123456' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/');
+    });
+  });
+
+  it('should not be able to reset password without token', async () => {
+    mockedUrlSearchString = '';
+
+    const { getByPlaceholderText, getByText } = render(<ResetPassword />);
+
+    const passwordField = getByPlaceholderText('Nova senha');
+    const passwordConfirmationField = getByPlaceholderText(
+      'Confirmação da senha',
+    );
+    const buttonElement = getByText('Alterar senha');
+
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+    fireEvent.change(passwordConfirmationField, {
+      target: { value: '123456' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalledWith('/');
+
+      expect(mockedAddToast).toHaveBeenCalledWith(
+        expect.objectContaining({
+          type: 'error',
+        }),
+      );
+    });
+  });
+
+  it('should not be able to reset password if new password not provided', async () => {
+    const { getByPlaceholderText, getByText } = render(<ResetPassword />);
+
+    const passwordField = getByPlaceholderText('Nova senha');
+    const passwordConfirmationField = getByPlaceholderText(
+      'Confirmação da senha',
+    );
+    const buttonElement = getByText('Alterar senha');
+
+    fireEvent.change(passwordField, { target: { value: '' } });
+    fireEvent.change(passwordConfirmationField, {
+      target: { value: '' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalledWith('/');
+    });
+  });
+
+  it('should not be able to reset password if confirmation does not match', async () => {
+    const { getByPlaceholderText, getByText } = render(<ResetPassword />);
+
+    const passwordField = getByPlaceholderText('Nova senha');
+    const passwordConfirmationField = getByPlaceholderText(
+      'Confirmação da senha',
+    );
+    const buttonElement = getByText('Alterar senha');
+
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+    fireEvent.change(passwordConfirmationField, {
+      target: { value: '123' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).not.toHaveBeenCalledWith('/');
+    });
+  });
+});
